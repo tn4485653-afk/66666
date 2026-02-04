d6d7070b772885770a8ef8d3021f15a2374dbfaa
tn4485653-afk <tn4485653@gmail.com>
2026-02-04 09:48:26 +0700

    Create api-config.js

---
+++ api-config.js
@@ -0,0 +1,15 @@
+// FILE CẤU HÌNH API - SIKIBIDI EXE
+const BOT_SETTINGS = {
+    // Đường dẫn gốc của API
+    apiBaseUrl: "https://siiyfcc.onrender.com",
+
+    // Danh sách các vật phẩm/emote
+    emotes: [
+        {id: "909000063", name: "AK RỒNG"},
+        {id: "909000068", name: "SCAR MẬP"},
+        {id: "909000075", name: "MP40 XÀ"},
+        {id: "909000081", name: "M1014 LONG"},
+        // Bạn có thể thêm súng mới vào đây cực dễ dàng:
+        // {id: "ID_MOI", name: "TÊN_SÚNG_MỚI"}
+    ]
+};
